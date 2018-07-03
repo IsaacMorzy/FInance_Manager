2a7652ac699a6b89045f544bd6a9397fcc2bfdd0
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-07-03 13:35:48 +0300

    Add view route function for login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template,redirect,url_for,flash,request
+from ..models import User
+from .forms import RegistrationForm,LoginForm
+from .. import db
+from flask_login import login_user,logout_user,login_required
 from . import auth
-
 @auth.route('/login')
 def login():
-    return render_template('auth/login.html')
+    login_form = LoginForm()
+    return render_template('auth/login.html'login_form=login_form)
