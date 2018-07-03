2772e573197279bbf7ddbf8236469a172ca3e6f8
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-07-03 11:31:32 +0300

    Add alchemy configurations

--- config.py
+++ config.py
@@ -1,45 +1,20 @@
 import os
 
 class Config:
-    '''
-    General configuration parent class
-    '''
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+  
+    SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://isaac:morzy@localhost/finance' #location of the database with authentication.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProdConfig(Config):
-    '''
-    Pruduction configuration child class
+    pass
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
-class TestConfig(Config):
-    '''
-    Testing configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings 
-    '''
-    SQLALCHEMY_DATABASE_URI=os.environ.get('SQLALCHEMY_DATABASE_URI')
 
 class DevConfig(Config):
-    '''
-    Development configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-   
-
     DEBUG = True
 
-
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig,
-    'test': TestConfig
+'development':DevConfig,
+'production':ProdConfig
 }
