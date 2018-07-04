13537b9bc7d0d477962d0f54c76f303e696867a8
Immanuel Mugambi <immamugambi@gmail.com>
2018-07-04 11:23:40 +0300

    Created upload photos configuration

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 class ProdConfig(Config):
